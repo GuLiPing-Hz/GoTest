077ed0c4f25c9f7e5fe40020ff2feeeee893de73
guliping <1058719078@qq.com>
2018-11-12 20:19:27 +0800

    11

--- sql/fish_1.2.2.sql
+++ sql/fish_1.2.2.sql
@@ -1,5 +1,6 @@
 #捕鱼数据库更新SQL
 #v1.2.2 sql
+#新增娱乐场配置
 CREATE TABLE IF NOT EXISTS ylc_cfg (
   #定义字段名 类型 默认值 主键 是否可空 自动增加
   id         BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT
@@ -12,5 +13,15 @@ CREATE TABLE IF NOT EXISTS ylc_cfg (
   DEFAULT CHARSET = utf8mb4;
 INSERT INTO ylc_cfg (gameId, vipLimit, levelLimit) VALUES (2, 2, 0);
 INSERT INTO ylc_cfg (gameId, vipLimit, levelLimit) VALUES (3, 0, 1);
+
+#更新捕鱼在线奖励日志表
+ALTER TABLE Buyu.online_reward_log
+  ADD insert_time datetime default NOW() not null
+comment '数据的插入时间';
+#创建索引
+create index insert_time
+  on Buyu.online_reward_log (insert_time);
+#改成前一天的记录
+UPDATE Buyu.online_reward_log SET insert_time = DATE_SUB(NOW(), INTERVAL '1 0:0:0' DAY_SECOND);
 
 
