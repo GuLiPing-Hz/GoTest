74734d11e842cf2a0bd20a7deb6e70a19395b614
guliping <1058719078@qq.com>
2019-09-16 00:34:04 +0800

    增加unsafe包的测试，3个实用函数,Sizeof,Alignof,Offsetof;reflect.DeepEqual能方便对比两个同类型数据是否完全一致，用于检测环状结构；但是对nil和空slice判断为false，对nil和空map也判断为false

--- go1/src/study/tgpl/lx12.11.go
+++ go1/src/study/tgpl/lx12.11.go
@@ -6,11 +6,20 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
+	"bytes"
+	"regexp"
 )
 
-func populate(v reflect.Value, val string) error {
+func populate(v reflect.Value, val, check string) error {
 	switch v.Kind() {
 	case reflect.String:
+		//练习题12.12
+		if check == "email" {
+			matched, _ := regexp.MatchString(`\w+@[\w.]+`, val)
+			if !matched {
+				return fmt.Errorf("the param need email,give %s", val)
+			}
+		}
 		v.SetString(val)
 	case reflect.Bool:
 		b, err := strconv.ParseBool(val)
@@ -30,20 +39,27 @@ func populate(v reflect.Value, val string) error {
 	return nil
 }
 
+type RValue struct {
+	val   reflect.Value
+	check string
+}
+
 func Unpack(r *http.Request, ptr interface{}) error {
 	if err := r.ParseForm(); err != nil {
 		return err
 	}
 
-	fields := make(map[string]reflect.Value)
+	fields := make(map[string]RValue)
 	v := reflect.ValueOf(ptr).Elem()
 	for i := 0; i < v.NumField(); i++ {
-		field := v.Type().Field(i)
-		name := field.Tag.Get("http")
+		fieldT := v.Type().Field(i)
+		name := fieldT.Tag.Get("http")
 		if name == "" {
-			name = strings.ToLower(field.Name)
+			name = strings.ToLower(fieldT.Name)
 		}
-		fields[name] = v.Field(i) //field不需要Elem
+		fields[name] = RValue{v.Field(i),
+			fieldT.Tag.Get("check"),
+		} //field不需要Elem
 	}
 
 	//for k, v := range fields {
@@ -59,14 +75,14 @@ func Unpack(r *http.Request, ptr interface{}) error {
 		}
 
 		for _, v := range vs {
-			if f.Kind() == reflect.Slice {
-				val := reflect.New(f.Type().Elem()).Elem()
-				if err := populate(val, v); err != nil {
+			if f.val.Kind() == reflect.Slice {
+				val := reflect.New(f.val.Type().Elem()).Elem()
+				if err := populate(val, v, ""); err != nil {
 					return err
 				}
-				f.Set(reflect.Append(f, val))
+				f.val.Set(reflect.Append(f.val, val))
 			} else {
-				if err := populate(f, v); err != nil {
+				if err := populate(f.val, v, f.check); err != nil {
 					return err
 				}
 			}
@@ -80,6 +96,7 @@ func hFReflect(w http.ResponseWriter, r *http.Request) {
 		Labels     []string `http:"l"`
 		MaxResults int      `http:"max"`
 		Exact      bool     `http:"x"`
+		Email      string   `http:"e" check:"email"`
 	}
 	data.MaxResults = 10 // set default
 	if err := Unpack(r, &data); err != nil {
@@ -90,7 +107,123 @@ func hFReflect(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Search: %+v\n", data)
 }
 
+func getStr(name string, v reflect.Value) (string, error) {
+	switch v.Kind() {
+	case reflect.Slice:
+		str := &strings.Builder{}
+		for i := 0; i < v.Len(); i++ {
+			s, err := getStr(name, v.Index(i))
+			if err != nil {
+				return "", err
+			}
+
+			if i > 0 {
+				str.WriteRune('&')
+			}
+			str.WriteString(s)
+		}
+		return str.String(), nil
+	case reflect.String:
+		return fmt.Sprintf("%s=%s", name, v.String()), nil
+	case reflect.Int:
+		return fmt.Sprintf("%s=%d", name, v.Int()), nil
+	case reflect.Bool:
+		return fmt.Sprintf("%s=%t", name, v.Bool()), nil
+	}
+	return "", fmt.Errorf("unsupport type =%d", v.Kind())
+}
+
+func Pack(data interface{}) ([]byte, error) {
+	v := reflect.ValueOf(data)
+	buf := &bytes.Buffer{}
+	for i := 0; i < v.NumField(); i++ {
+		fieldT := v.Type().Field(i)
+		fieldV := v.Field(i)
+		name := fieldT.Tag.Get("http")
+		if name == "" {
+			name = strings.ToLower(fieldT.Name)
+		}
+
+		str, err := getStr(name, fieldV)
+		if err != nil {
+			return nil, err
+		}
+
+		if i > 0 {
+			buf.WriteRune('&')
+		}
+		buf.WriteString(str)
+	}
+
+	return buf.Bytes(), nil
+}
+
+func lx12_11() {
+	var data = struct {
+		Labels     []string `http:"l"`
+		MaxResults int      `http:"max"`
+		Exact      bool     `http:"x"`
+	}{
+		[]string{"a", "b"},
+		5,
+		true,
+	}
+	buf, err := Pack(data)
+	if err != nil {
+		fmt.Printf("err=%s\n", err.Error())
+	}
+
+	fmt.Printf("param=[%s]\n", string(buf))
+}
+
+func showReflectFunc(x interface{}) {
+	fmt.Printf("type = %T\n", x)
+
+	t := reflect.TypeOf(x)
+	v := reflect.ValueOf(x)
+	fmt.Printf("t==t2 %t\n", t == v.Type())
+
+	for i := 0; i < v.NumMethod(); i++ {
+		methodT := t.Method(i)
+		methodV := v.Method(i)
+		fmt.Printf("(%T).%s=%s\n", x, methodT.Name, methodT.Type.String())
+
+		//使用Value调用函数，默认自带了当前实例，我们只需传递形参即可
+		results := methodV.Call([]reflect.Value{reflect.ValueOf("Jack")})
+		for i, v := range results {
+			fmt.Printf("call1 result[%d]=%v\n", i, v)
+		}
+
+		//使用Type调用函数，相当于调用c的静态函数，但是我们需要把实例传入，类似Lua的self机制
+		results2 := methodT.Func.Call([]reflect.Value{v, reflect.ValueOf("Jack")})
+		for i, v := range results2 {
+			fmt.Printf("call2 result[%d]=%v\n", i, v)
+		}
+	}
+}
+
+type HelloW struct {
+}
+
+func (h *HelloW) Print(x string) string {
+	return fmt.Sprintf("Hello,I'm %s", x)
+}
+
+/**
+反射的练习以及实验已经到头了，最后值得注意的是反射最好不要去滥用，相对于普通的写法，
+反射无法让编译器识别类型错误，也影响了自动化重构和分析工具的准确性，
+而且反射在运行速度有劣势，要慢一到两个数量级，
+这就决定了关键核心部分代码不能用反射去偷懒实现。
+原话参见《the Go programming language》458页
+
+最多用于测试的时候可以用一下。
+ */
 func main() {
+	//showReflectFunc(time.Hour)
+	showReflectFunc(&HelloW{})
+
+	lx12_11()
+
 	http.HandleFunc("/reflect", hFReflect)
 	if err := http.ListenAndServe(":8000", nil); err != nil {
 		fmt.Printf("err=%s\n", err)
---
+++ go1/src/study/tgpl/lx13.1.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"fmt"
+	"unsafe"
+	"reflect"
+)
+
+/**
+各个底层类型的对齐字节数
+
+bool 1个字节
+intN, uintN, floatN, complexN N/8个字节(例如float64是8个字节)
+//上面的是按字节，下面按机器字(32位就对应4字节，64位对应8字节)
+
+int, uint, uintptr 1个机器字
+*T 1个机器字
+string 2个机器字(data,len)
+[]T 3个机器字(data,len,cap)
+map 1个机器字
+func 1个机器字
+chan 1个机器字
+interface 2个机器字(type,value)
+ */
+type X struct {
+	//64位机器
+	a bool    //1字节
+	b float64 //对齐空7字节+8字节
+	c int16   //2字节 +补齐6字节
+}
+
+type X2 struct {
+	a bool    //1字节
+	c int16   //对齐空1字节 2字节
+	b float64 //对齐空4字节 + 8字节
+}
+
+func isIntX(v reflect.Value) bool {
+	return v.Kind() == reflect.Int || v.Kind() == reflect.Int8 ||
+		v.Kind() == reflect.Int16 || v.Kind() == reflect.Int32 ||
+		v.Kind() == reflect.Int64
+}
+
+func isUintX(v reflect.Value) bool {
+	return v.Kind() == reflect.Uint || v.Kind() == reflect.Uint8 ||
+		v.Kind() == reflect.Uint16 || v.Kind() == reflect.Uint32 ||
+		v.Kind() == reflect.Uint64
+}
+
+func lx13_1(x, y interface{}) bool {
+	v1 := reflect.ValueOf(x)
+	v2 := reflect.ValueOf(y)
+
+	if !isIntX(v1) || !isIntX(v2) {
+		return false
+	}
+	return v1.Int() == v2.Int()
+}
+
+//数据相同的两个结构，X2占用的内存更小。
+func main() {
+	x := X{}
+	x2 := X2{}
+	//打印实际占用字节大小
+	fmt.Printf("Sizeof(x)=%d\n", unsafe.Sizeof(x))
+	fmt.Printf("Sizeof(x2)=%d\n", unsafe.Sizeof(x2))
+	fmt.Printf("Sizeof(x.a)=%d\n", unsafe.Sizeof(x.a))
+	fmt.Printf("Sizeof(x.b)=%d\n", unsafe.Sizeof(x.b))
+	fmt.Printf("Sizeof(x.c)=%d\n", unsafe.Sizeof(x.c))
+
+	//打印对齐大小
+	fmt.Printf("Alignof(x.a)=%d\n", unsafe.Alignof(x.a))
+	fmt.Printf("Alignof(x.b)=%d\n", unsafe.Alignof(x.b))
+	fmt.Printf("Alignof(x.c)=%d\n", unsafe.Alignof(x.c))
+
+	//打印字段在结构中的偏移位置
+	fmt.Printf("Offsetof(x.a)=%d\n", unsafe.Offsetof(x.a))
+	fmt.Printf("Offsetof(x.b)=%d\n", unsafe.Offsetof(x.b))
+	fmt.Printf("Offsetof(x.c)=%d\n", unsafe.Offsetof(x.c))
+	fmt.Printf("Offsetof(x2.a)=%d\n", unsafe.Offsetof(x2.a))
+	fmt.Printf("Offsetof(x2.b)=%d\n", unsafe.Offsetof(x2.b))
+	fmt.Printf("Offsetof(x2.c)=%d\n", unsafe.Offsetof(x2.c))
+
+	fmt.Println()
+	var a = float64(1)
+
+	//通过对unsafe.Pointer的桥接，我们可以将任意类型指针转换成其他类型的指针
+	//unsafe.Pointer可以理解成c语言里面的void*指针
+	var b = (*int64)(unsafe.Pointer(&a))
+	fmt.Printf("a=%b,b=%016x\n", a, *b)
+
+	fmt.Println("练习题13.1", reflect.DeepEqual(int16(5), int32(5)),
+		lx13_1(int16(5), int32(5)))
+}
