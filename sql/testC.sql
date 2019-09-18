fe6577146b96552199983b31bd7d039f3ecb6151
guliping <1058719078@qq.com>
2019-09-18 19:06:41 +0800

    新增渠道表

--- sql/testC.sql
+++ sql/testC.sql
@@ -1,4 +1,13 @@
 select sum(money) as s from pay_log where uid=165272
 		and result=0 and channel in(1,2,3) and addtime>='2019-09-15'
+create table flavor_cfg
+(
+    flavor varchar(50) not null
+        primary key
+);
 
+INSERT INTO `Buyu`.`flavor_cfg` (`flavor`) VALUES ('MARKET_xw');
+INSERT INTO `Buyu`.`flavor_cfg` (`flavor`) VALUES ('MARKET_xw1');
+INSERT INTO `Buyu`.`flavor_cfg` (`flavor`) VALUES ('MARKET_sp');
+INSERT INTO `Buyu`.`flavor_cfg` (`flavor`) VALUES ('AOfficial');
 
