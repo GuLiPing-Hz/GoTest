fed8522864352b632b2b5ee744e74db6957482ee
guliping <1058719078@qq.com>
2019-09-20 15:07:21 +0800

    新增slot sql语句

--- sql/testSlot.sql
+++ sql/testSlot.sql
@@ -1,25 +1,3 @@
-create table coin_change_today
-(
-    uid         bigint not null
-        primary key,
-    coin_change bigint null
-);
-
-create index coin_change_today_uid_index
-    on coin_change_today (uid);
-
-create table fee_log
-(
-    uid bigint   not null,
-    tm  datetime not null,
-    fee bigint   null comment '手续费',
-    primary key (uid, tm)
-);
-create index fee_log_uid_index
-    on fee_log (uid);
-create index fee_log_tm_index
-    on fee_log (tm);
-
-insert into coin_change_today(uid, coin_change) value (?, ?)
-                on duplicate key update coin_change = ?;
-| args=[165272 -15000 -15000] RowsAffected error=not 1 row affected
+insert into stock(stock_normal, stock_large, fee_normal, fee_large, bibei_normal, bibei_large, addTime, updateTime)
+values (0, 0, 0, 0, 0, 0 ,'2019-09-20 10:26:36', '2019-09-20 10:26:36')
+# Prepare error = Error 1136: Column count doesn 't match value count at row 1
